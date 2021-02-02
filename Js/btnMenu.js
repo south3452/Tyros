c644828e85bac5cac9439583ef6b1c85a4d09ca3
MohamedSantos <mohamedprofissional22@gmail.com>
2021-02-02 01:22:12 -0300

    arrumando responsividade

--- Js/btnMenu.js
+++ Js/btnMenu.js
@@ -1,12 +1,15 @@
 const $menu = document.querySelector('.menu')
+const $hidden = document.querySelector('.center')
 const $btnMenuOpen = document.querySelector('.btnMenu_open')
 const $btnMenuClose = document.querySelector('.btnMenu_close')
 
 $btnMenuOpen.addEventListener('click', function() {
     $menu.classList.add('menu_open')
+    $hidden.classList.add('hidden')
 })
 
 $btnMenuClose.addEventListener('click', function() {
     $menu.classList.remove('menu_open')
+    $hidden.classList.remove('hidden')
 })
 
