72d772ab82ddb5b4ac5a118e5db06ba7b88c9562
MohamedSantos <mohamedprofissional22@gmail.com>
2021-02-15 03:02:52 -0300

    mudança pra node

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const nodemailer = require("nodemailer")
+const app = express()
+
+app.set('view engine','ejs')
+
+app.use(express.static(__dirname + '/public'))
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+
+let transporter = nodemailer.createTransport({
+    host:"smtp.gmail.com",
+    port:587,
+    secure: false,
+    auth:{
+        user: "tyrossoftwares@gmail.com",
+        pass: "niviadino"
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+})
+
+app.post("/sendEmail",(req,res) =>{
+    const mailOption = {
+        from: "CONTATO <tyrossoftwares@gmail.com>",
+        to: "wavetheproducerofc@gmail.com",
+        subject:"Nome:" + req.body.nome +" E-mail: " + req.body.email,
+        text:"Texto do email",
+        html: "Assunto: " + req.body.assunto + "<br>" + "Telefone: " + req.body.tel + "<br>" + req.body.texto
+    }
+    transporter.sendMail(mailOption, function(err, info){
+        if(err)
+            console.log(err)
+        else
+            console.log("ENVIADO");
+    })
+})
+
+app.get("/",(req,res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+app.listen(3000,()=>{
+    console.log("Rodando")
+})
